08288101fee10be614a9720a997728deadf744d8
Joshua <jpe7619@gmail.com>
2023-09-26 19:26:59 -0700

    update fetch to axios

--- src/disneyland.js
+++ src/disneyland.js
@@ -1,38 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function DisneylandUSA() {
-  const [times, setTimes] = useState(null);
+    axios.get('http://localhost:3001/api/data')
+    .then((response) => {
+      console.log(response, "this is the response")
+    })
+    .catch((error) =>
+    console.log(error, "this is an error"))
 
-  const fetchData = async () => {
-    const url = "http://queue-times.com/parks/16/queue_times.json";
+    return (
+      <>
+      </>
+    )
 
-    const response = await fetch(url, {
-      method: "GET",
-      mode: "cors",
-      referrerPolicy: "strict-origin-when-cross-origin"
-    });
-    if (!response.ok) {
-      throw new Error(
-        `Failed to fetch park data. Status Code: ${response.status}`
-      );
-    }
-    const data = await response.json();
+  }
 
-    console.log(data);
-
-    setTimes(data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-      <p>
-        Powered by <a href="https://queue-times.com/en-US">Queue-Times.com</a>
-      </p>
-    </div>
-  );
-}
 export default DisneylandUSA;
